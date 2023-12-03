381bf0969ea0a0e534550456d1e7d6adac014c26
Matteo Crespi <crespi.matteo@icloud.com>
2023-12-03 02:53:35 +0100

    wip

--- Core/Sources/Services/SetupClient.swift
+++ Core/Sources/Services/SetupClient.swift
@@ -6,21 +6,23 @@ public struct SetupClient {
 }
 
 public extension SetupClient {
+  private enum UnexpectedError: Error {
+    case groupNotFound
+  }
+
   init(fileManager: FileManagerProtocol, networkService: NetworkService, persistenceService: PersistenceService) {
     perform = {
-      let applicationSupportURL = fileManager.containerURL(
-        forSecurityApplicationGroupIdentifier: "group.com.mttcrsp.ansia"
-      )!
-      let applicationDatabaseURL = applicationSupportURL
+      guard let applicationDatabaseURL = fileManager
+        .containerURL(forSecurityApplicationGroupIdentifier: "group.com.mttcrsp.ansia")?
         .appendingPathComponent("db")
         .appendingPathExtension("sqlite")
+      else { throw UnexpectedError.groupNotFound }
+
       try persistenceService.load(at: applicationDatabaseURL.path)
 
       let feedsRead = GetFeedsByCollection(collection: .main)
       let feeds = try persistenceService.performSync(feedsRead)
-      guard feeds.isEmpty else {
-        return
-      }
+      guard feeds.isEmpty else { return }
 
       let feedsRequest = FeedsRequest()
       let feedsResponse = try await networkService.perform(feedsRequest)
--- Widget/Sources/Widget.swift
+++ Widget/Sources/Widget.swift
@@ -6,8 +6,11 @@ import WidgetKit
 @main
 struct AnsiaWidget: Widget {
   var body: some WidgetConfiguration {
-    StaticConfiguration(kind: "article", provider: ArticlesProvider()) { entry in
+    StaticConfiguration(kind: "article", provider: ArticlesProvider(feedSlug: .main)) { entry in
       WidgetView(entry: entry)
+        .backport.widgetBackground(
+          DesignSystemAsset.background.swiftUIColor
+        )
     }
     .configurationDisplayName("display name")
     .description("description")
@@ -15,50 +18,76 @@ struct AnsiaWidget: Widget {
 }
 
 struct WidgetView: View {
+  private static let formatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .none
+    formatter.timeStyle = .short
+    return formatter
+  }()
+
   let entry: ArticlesProvider.Entry
   var body: some View {
-    VStack(alignment: .leading, spacing: 0) {
-      Text("Top Stories")
-        .font(FontFamily.NYTFranklin.bold.swiftUIFont(size: 14))
-      Spacer(minLength: 8)
-      Text(AttributedString(PublishedAtFormatter().attributedString(for: entry.items[0].publishedAt)))
-        .padding(.bottom, 4)
-      Text(entry.items[0].title)
-        .font(FontFamily.NYTCheltenham.bold.swiftUIFont(size: 16))
-        .lineLimit(2)
-        .padding(.bottom, 4)
-        .widgetURL(URL(string: ""))
-      Divider()
-        .padding(.bottom, 4)
-      Text(entry.items[1].title)
-        .font(FontFamily.NYTCheltenham.book.swiftUIFont(size: 12))
-        .lineLimit(2)
-        .widgetURL(URL(string: ""))
-      Spacer(minLength: 0)
+    switch entry.result {
+    case .failure:
+      Text("Qualcosa é andato storto")
+        .font(FontFamily.NYTFranklin.bold.swiftUIFont(size: 17))
+        .foregroundStyle(DesignSystemAsset.secondaryLabel.swiftUIColor)
+        .textCase(.uppercase)
+    case let .success((title, items)):
+      VStack(alignment: .leading, spacing: 0) {
+        HStack(alignment: .lastTextBaseline) {
+          Text(title)
+            .font(FontFamily.NYTFranklin.bold.swiftUIFont(size: 15))
+          Spacer()
+          Text("alle \(Self.formatter.string(from: entry.date))")
+            .font(FontFamily.NYTFranklin.medium.swiftUIFont(size: 12))
+            .foregroundStyle(DesignSystemAsset.live.swiftUIColor)
+        }
+        .padding(.bottom, 8)
+        Divider()
+          .padding(.bottom, 8)
+        ForEach(Array(items.enumerated()), id: \.element.id) { index, item in
+          Text(item.title)
+            .font(
+              index == 0
+                ? FontFamily.NYTCheltenham.bold.swiftUIFont(size: 17)
+                : FontFamily.NYTCheltenham.book.swiftUIFont(size: 15)
+            )
+            .lineLimit(2)
+            .padding(.bottom, 4)
+            .widgetURL(URL(string: ""))
+        }
+        Spacer(minLength: 0)
+      }
+      .foregroundStyle(DesignSystemAsset.label.swiftUIColor)
     }
-    .foregroundStyle(DesignSystemAsset.label.swiftUIColor)
   }
 }
 
 struct ArticlesProvider: TimelineProvider {
   struct Entry: TimelineEntry {
-    struct Item {
-      var title: String
-      var publishedAt: Date
-    }
-
-    var date: Date
-    var items: [Item]
+    var date = Date()
+    var result: Result<(String, [Item]), Error>
   }
 
+  struct Item {
+    var id: String
+    var title: String
+  }
+
+  enum UnexpectedError: Error {
+    case dispatchGroupFailure
+    case groupNotFound
+    case feedNotFound
+  }
+
+  let feedSlug: Feed.Slug
+
   func placeholder(in _: Context) -> Entry {
-    .init(
-      date: Date(),
-      items: [
-        .init(title: "A $52,112 Air Ambulance Ride: Coronavirus Patients Battle Surprise Bills", publishedAt: Date().addingTimeInterval(-100)),
-        .init(title: "The Day in Polls: More Clarity in North Carolina", publishedAt: Date().addingTimeInterval(-200)),
-      ]
-    )
+    .init(result: .success(("Principali", [
+      .init(id: "1", title: "La Croazia adotta l'euro ed entra nell'area Schengen"),
+      .init(id: "2", title: "Meta, milioni di persone continuano a usare Instagram in Iran"),
+    ])))
   }
 
   func getSnapshot(in context: Context, completion: @escaping (Entry) -> Void) {
@@ -67,13 +96,11 @@ struct ArticlesProvider: TimelineProvider {
 
   func getTimeline(in _: Context, completion: @escaping (Timeline<Entry>) -> Void) {
     do {
-      let fileManager = FileManager.default
-      let applicationSupportURL = fileManager.containerURL(
-        forSecurityApplicationGroupIdentifier: "group.com.mttcrsp.ansia"
-      )!
-      let applicationDatabaseURL = applicationSupportURL
+      guard let applicationDatabaseURL = FileManager.default
+        .containerURL(forSecurityApplicationGroupIdentifier: "group.com.mttcrsp.ansia")?
         .appendingPathComponent("db")
         .appendingPathExtension("sqlite")
+      else { throw UnexpectedError.groupNotFound }
 
       let migrations: [PersistenceServiceMigration] = [
         CreateArticlesTable(),
@@ -82,61 +109,71 @@ struct ArticlesProvider: TimelineProvider {
         CreateRecentsTable(),
       ]
 
-      let query = GetArticlesByFeed(slug: .main, limit: 2)
       let persistenceService = PersistenceServiceLive(migrations: migrations)
       try persistenceService.load(at: applicationDatabaseURL.path())
-      persistenceService.perform(query) { result in
-        switch result {
-        case .failure:
-          completion(
-            .init(
-              entries: [
-                .init(date: Date(), items: [
-                  .init(title: "A $52,112 Air Ambulance Ride: Coronavirus Patients Battle Surprise Bills", publishedAt: Date().addingTimeInterval(-100)),
-                  .init(title: "The Day in Polls: More Clarity in North Carolina", publishedAt: Date().addingTimeInterval(-200)),
-                ]),
-              ],
-              policy: .atEnd
-            )
+
+      Task {
+        do {
+          let networkService = NetworkServiceLive(urlSession: .shared)
+          let response = try await networkService.perform(
+            ArticlesByFeedRequest(slug: feedSlug)
           )
-        case let .success(articles):
-          print(articles)
-          completion(
-            .init(
-              entries: [
-                .init(
-                  date: Date(),
-                  items: articles.map { article in
-                    .init(title: article.title, publishedAt: article.publishedAt)
-                  }
-                ),
-              ],
-              policy: .atEnd
-            )
+          try await persistenceService.perform(
+            UpdateFeedArticles(slug: feedSlug, articles: response.articles)
           )
-//          completion(
-//            .init(
-//              entries: [
-//                .init(date: Date(), items: feed),
-//              ],
-//              policy: .atEnd
-//            )
-//          )
+        } catch { /* do nothing */ }
+
+        let result: Result<(String, [Item]), Error>
+        do {
+          let feedQuery = GetFeedBySlug(slug: feedSlug)
+          let feed = try await persistenceService.perform(feedQuery)
+          let articlesQuery = GetArticlesByFeed(slug: feedSlug, limit: 2)
+          let articles = try await persistenceService.perform(articlesQuery)
+          if let feed {
+            result = .success((feed.title, articles.map(Item.init)))
+          } else {
+            result = .failure(UnexpectedError.feedNotFound)
+          }
+        } catch {
+          result = .failure(error)
         }
+        completion(.init(entries: [.init(result: result)], policy: .atEnd))
       }
     } catch {
-      print(error)
-      completion(
-        .init(
-          entries: [
-            .init(date: Date(), items: [
-              .init(title: "A $52,112 Air Ambulance Ride: Coronavirus Patients Battle Surprise Bills", publishedAt: Date().addingTimeInterval(-100)),
-              .init(title: "The Day in Polls: More Clarity in North Carolina", publishedAt: Date().addingTimeInterval(-200)),
-            ]),
-          ],
-          policy: .atEnd
-        )
-      )
+      completion(.init(entries: [.init(result: .failure(error))], policy: .atEnd))
+    }
+  }
+}
+
+extension ArticlesProvider.Item: Identifiable {}
+
+private extension ArticlesProvider.Item {
+  init(article: Article) {
+    self.init(id: article.articleID.rawValue, title: article.title)
+  }
+}
+
+struct Backport<Content> {
+  let content: Content
+  init(_ content: Content) {
+    self.content = content
+  }
+}
+
+extension View {
+  var backport: Backport<Self> {
+    Backport(self)
+  }
+}
+
+extension Backport where Content: View {
+  @ViewBuilder func widgetBackground(_ backgroundView: some View) -> some View {
+    if #available(iOSApplicationExtension 17.0, *) {
+      content.containerBackground(for: .widget) {
+        backgroundView
+      }
+    } else {
+      content.background(backgroundView)
     }
   }
 }
